dc95046e87e9527fa5283e3fa0024271e9ada893
Chnapak <matej.hladecek@gmail.com>
2024-07-12 21:46:01 +0200

    Created repository and added a single comment.

---
+++ CountTheDigits/CountTheDigits.cs
@@ -0,0 +1,27 @@
+﻿namespace CountTheDigits
+{
+    internal class CountTheDigits
+    {
+        public void Run()
+        {
+            // Code is supposed to count the total appearences of each digit in a number, pretty simple.
+
+            string number = "193015803958013958013985013";
+            int[] digits = new int[10];
+            for (int i = 0; i < number.Length; i++) {
+                digits[SafelyConvertToInt(number[i])]++;
+            }
+
+            for (int i = 0; i < digits.Length; i++)  {
+                Console.WriteLine($"{i}: {digits[i]}" );
+            }
+        }
+        public int SafelyConvertToInt(char s) {
+            if (int.TryParse(s.ToString(), out int result))
+            {
+                return result;
+            }
+            return 0;
+        }
+    }
+}
